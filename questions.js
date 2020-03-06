a566ea9ca007d12986ff265e05bec7d32240baa4
“Ronny <contact@ronnyperez.com>
2020-03-06 16:42:50 -0600

    edit html, fixed script.js and linked with questions.

--- questions.js
+++ questions.js
@@ -1,24 +1,37 @@
 var questions = [
     {
         question: "What does HTML stand for?",
-        choices: ["test1","test2","test3","test4"],
-        answer: "test1"
+        choices: ["Hyper Text Markup Language", "High Text Markup Language", "Hyper Tabular Markup Language", "	None of these"],
+        answer: "Hyper Text Markup Language",
+    },
+    {
+        question: "Who is making the Web Standards?",
+        choices: ["Microsoft", "Apple", "Mozilla", "The World Wide Web Consortium"],
+        answer: "The World Wide Web Consortium",
+    },
+    {
+        question: "Which of the following jQuery method sets attributes of an element?",
+        choices: ["attr(name, value)", " setAttr(name, value)", "setAttributes(name, value)", "none of the above"],
+        answer: "attr(name, value)",
+    },
+    {
+        question: "What is the correct sequence of HTML tags for starting a webpage?",
+        choices: ["Title, Head, HTML", "HTML, Head, Title", "Head, Title, HTML", "none of the above"],
+        answer: "HTML, Head, Title",
     },
     {
         question: "What does CSS stand for?",
-        choices: ["test1","test2","test3","test4"],
-        answer: "test2"
+        choices: ["Computer Style Sheets", "Colorful Style Sheets", "Creative Style Sheets", "Cascading Style Sheets"],
+        answer: "Cascading Style Sheets",
     },
     {
-        question: "What does HTML stand for?",
-        choices: ["test1","test2","test3","test4"],
-        answer: "test1"
+        question: "Which property is used to change the background color?",
+        choices: ["Color", "bgColor", "Back-ground", "Background-color"],
+        answer: "Background-color",
     },
     {
-        question: "What does CSS stand for?",
-        choices: ["test1","test2","test3","test4"],
-        answer: "test2"
+        question: "Which of the following function of Array object returns a string representing the array and its elements?",
+        choices: ["toSource()", "sort()", "splice()", "toString()"],
+        answer: "toString()",
     },
-
-]
-console.log(questions);
+];
--- script.js
+++ script.js
@@ -1,52 +1,175 @@
-var currentquestionindex = 0;
-var time = questions.length * 15;
-var timeId;
+var currentQuestion = 0;
+var initialsInput = $("#initials-text");
+var initialsArray = [];
+var secondsLeft = (questions.length) * 15;
+var xInterval = null;
+var newInitials = null;
 
+// Start the timer and shows the content of the quiz
 
-var clock = document.getElementById("clock");
-var questionQuiz = document.getElementById("quiz");
-var startBtn = document.getElementById("startQuiz");
-var score = document.getElementById("score");
-var questionSubmit = document.getElementById("results");
+$(".start").click(function () {
+  setTimer();
+  $(".start").hide();
+  $(".quiz").show();
+  showQuestion();
+});
 
-startBtn.addEventListener("click", function () {
-   var timeId = setInterval(timer, 1000)
-})
+// Timer function
 
-function timer() {
-    time--;
-    clock.textContent = time;
-
-};
-
-
-
-// whatTheUserClicked = a string ""
-function nextQuestion(userChoice) {
-    var correctText = questions[currentQuestionIndex].answer;
-  
-    if (userChoice === correctText) {
-      console.log("Correct!");
-      score++;
+function setTimer() {
+  xInterval = setInterval(function () {
+    $("#timer").html(secondsLeft);
+    secondsLeft--;
+    if (secondsLeft <= 0) {
+      clearInterval(xInterval);
+      timeUp();
     }
-    else {
-      console.log("Wrong answer.");
-    }
-    currentQuestionIndex++;
-    getNewQuestion(currentQuestionIndex);
-  
+  }, 1000);
+}
+
+//clicking on the start quiz button fires howQuestion() function
+
+function showQuestion() {
+  var choices = questions[currentQuestion].choices;
+  var question = questions[currentQuestion].question;
+  $(".quiz h2").text(question);
+  $(".quiz ul").html("");
+  for (var i = 0; i < parseInt(choices.length); i++) {
+    var show = questions[currentQuestion].choices[i];
+    $(".quiz ul").append(`<li class="button-select" id="${i}">${show}</li>`);
   }
 
+  // comparing user's guests with correct answer with on-click if/else statement. Feedback is appended and styled.
 
-  function answerClicked() {
-    var a = document.getElementById("choiceA");
-    var b = document.getElementById("choiceB");
-    var c = document.getElementById("choiceC");
-    var d = document.getElementById("choiceD");
-  
-    a.addEventListener("click", function () { nextQuestion(a.innerText); });
-    b.addEventListener("click", function () { nextQuestion(b.innerText); });
-    c.addEventListener("click", function () { nextQuestion(c.innerText); });
-    d.addEventListener("click", function () { nextQuestion(d.innerText); });
+  $("li").click(function () {
+    var guessid = $(this).attr("id");
+    var guess = questions[currentQuestion].choices[guessid];
+    var answer = questions[currentQuestion].answer;
+
+    // checking if user's guess matches correct answer
+
+    if (answer === guess) {
+      $(".feedback").fadeIn(200);
+      $(".feedback")
+        .html("<h4>Correct!<h4>")
+        .fadeOut(900);
+      $(".feedback").css({
+        color: "green",
+        "text-align": "center",
+        "border-top": "lightgrey",
+        "border-top-width": "1px",
+        "border-top-style": "solid"
+      });
+      currentQuestion++;
+      showScorePage();
+
+    } else {
+      $(".feedback").fadeIn(200);
+      $(".feedback")
+        .html("<h4>Wrong!<h4>")
+        .fadeOut(900);
+      $(".feedback").css({
+        color: "red",
+        "text-align": "center",
+        "border-top": "grey",
+        "border-top-width": "1px",
+        "border-top-style": "solid"
+      });
+
+       // penalizes player by 10 seconds for each wrong answer
+
+      secondsLeft = secondsLeft -= 10;
+      currentQuestion++;
+      showScorePage();
+    }
+  });
+}
+
+
+function showScorePage() {
+  if (currentQuestion < questions.length) {
+    showQuestion();
+  } else {
+    $("#timer").remove();
+    $(".quiz").hide();
+    $(".scoreContainer").show();
+    $("#scoreNumber").append(secondsLeft);
+    clearInterval(xInterval);
   }
-  answerClicked();
+}
+
+function timeUp() {
+  $("#timer").text("expired!");
+  $(".quiz").hide();
+  $(".scoreContainer").show();
+  $("#scoreNumber").append(secondsLeft);
+  clearInterval(xInterval);
+}
+
+// Creates the 3 local storage functions
+
+$("#submit-initials").click(function(e) {
+  e.preventDefault();
+  loadScores();
+  saveScores();
+  showScores();
+  highScoresPage();
+});
+
+// 1 - Local storage for savin user init and score.
+
+function saveScores() {
+  var scoreName = initialsInput.val();
+  var highScores = scoreName + " : " + secondsLeft;
+  initialsArray.push(highScores);
+
+  initialsArray.sort(function(a, b){return b-a}); 
+  localStorage.setItem("listOfItems", JSON.stringify(initialsArray));
+}
+
+// 2 - second local storage
+
+function loadScores() {
+  var savedScores = localStorage.getItem("listOfItems");
+  var allScores = JSON.parse(savedScores);
+
+  // If score were retrieved from local storage we need to update initialsArray to it
+  
+  if (allScores != null) {
+    initialsArray = allScores;
+  }
+}
+
+//3 - For each new result submitted we create a new line and append that to ordered list on #highScorePage
+
+function showScores() {
+  for (i = 0; i < initialsArray.length; i++) {
+    newInitials = $("<li></li>").append(initialsArray[i]);
+    $("#scoreList").append(newInitials);    
+  }
+}
+
+// 4 - After all 3 local storage functions run show the new div
+
+function highScoresPage() {
+  $(".highScorePage").show();
+  $("#initialsArray").hide();
+}
+
+// Function to see and hide highScorePage div
+
+$("#view-highscores").click(function(){
+  $(".highScorePage").toggle();
+});
+
+// functions for each of the "try again" and "clear highscores"
+
+$("#go-back").click(function () {
+  window.location.reload();
+});
+
+$("#clear").click(function (e) {
+  e.preventDefault();
+  $("#scoreList").css('display', 'none');
+  localStorage.clear();
+});
